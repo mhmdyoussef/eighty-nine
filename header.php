9fa1b329c8e56b8ee7c404c8b3fb48a0ec0dfe26
Mohamed Youssef <egsyntax@gmail.com>
2021-07-12 03:55:03 +0200

    Header created

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> dir="<?php bloginfo( 'text_direction' ); ?>">
+    <head>
+        <?php wp_head(); ?>
+        <title><?php bloginfo( 'name' ); ?></title>
+    </head>
+
+    <body>
